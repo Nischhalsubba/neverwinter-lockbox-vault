c4d5b8228e8194bb7b9d340e52e8231286311e53
Nischhal Raj Subba <hinischalsubba@gmail.com>
2026-07-13 09:53:48 +0545

    Add NW Hub media map

---
+++ data/nwhub-media.js
@@ -0,0 +1,15 @@
+export default {
+  source: 'https://nw-hub.com/packs',
+  generatedAt: null,
+  stats: {
+    assetsDiscovered: 0,
+    candidatesMatched: 0,
+  },
+  items: {
+    lockbox: {},
+    companion: {},
+    mount: {},
+    artifact: {},
+    race: {},
+  },
+};
