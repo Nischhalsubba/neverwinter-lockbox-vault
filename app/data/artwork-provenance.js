73affbdc03fa29830ac9754e5958e273938e6244
Nischhal Raj Subba <hinischalsubba@gmail.com>
2026-08-12 20:30:41 +0545

    Record artwork provenance

---
+++ app/data/artwork-provenance.js
@@ -0,0 +1,35 @@
+export default {
+  curated: {
+    companion: {
+      'Sardina the Tressym': {
+        sourceUrl: 'https://forgottenrealms.fandom.com/wiki/Sardina',
+        provider: 'Forgotten Realms Wiki',
+        localPath: '/assets/rewards/curated/sardina-the-tressym.png',
+      },
+    },
+    mount: {
+      'Cactus the Hedgehog': {
+        sourceUrl: 'https://forgottenrealms.fandom.com/wiki/Cactus_(hedgehog)',
+        provider: 'Forgotten Realms Wiki',
+        localPath: '/assets/rewards/curated/cactus-the-hedgehog.png',
+      },
+      "Hag's Hexing Cauldron": {
+        sourceUrl: 'https://neverwinter.fandom.com/wiki/Hag%27s_Hexing_Cauldron',
+        provider: 'Neverwinter Wiki',
+        localPath: '/assets/rewards/curated/hags-hexing-cauldron.png',
+      },
+    },
+    race: {
+      'Sigil of the Metallic Ancestry Dragonborn': {
+        sourceUrl: 'https://neverwinter.fandom.com/wiki/Sigil_of_the_Metallic_Ancestry_Dragonborn',
+        provider: 'Neverwinter Wiki',
+        localPath: '/assets/rewards/curated/metallic-ancestry-dragonborn.png',
+      },
+      'Glorious Resurgence Legendary Pack': {
+        sourceUrl: 'https://neverwinter.fandom.com/wiki/Glorious_Resurgence_Legendary_Pack',
+        provider: 'Neverwinter Wiki',
+        localPath: '/assets/rewards/curated/glorious-resurgence-legendary-pack.png',
+      },
+    },
+  },
+};
