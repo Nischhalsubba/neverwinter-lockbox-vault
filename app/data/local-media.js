d5a41ea2b896205bebba3b22f372638ff6ede627
Nischhal Raj Subba <hinischalsubba@gmail.com>
2026-08-12 18:22:52 +0545

    Add local media catalog

---
+++ app/data/local-media.js
@@ -0,0 +1,15 @@
+export default {
+  generatedAt: null,
+  stats: {
+    lockboxesResolved: 0,
+    rewardsResolved: 0,
+    unresolved: 0,
+  },
+  items: {
+    lockbox: {},
+    companion: {},
+    mount: {},
+    artifact: {},
+    race: {},
+  },
+};
